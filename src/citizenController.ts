be62724fe245c43da038d51bdc27c20f6c492a28
Arek <arekkarwasz@wp.pl>
2023-12-02 09:51:01 +0100

    comment all

--- src/citizenController.ts
+++ src/citizenController.ts
@@ -29,39 +29,39 @@ const registerCitizen = async (
   referrerAddress: string
 ) => {
   const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || "", provider);
-  const reputationRegistryContract = new ethers.Contract(
-    "0x4D280BEBD203d9BbDaE2bBc4f0F25C64fB198613",
-    ReputationRegistryABI.abi,
-    wallet
-  );
+  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || "", provider);
+  // const reputationRegistryContract = new ethers.Contract(
+  //   "0x4D280BEBD203d9BbDaE2bBc4f0F25C64fB198613",
+  //   ReputationRegistryABI.abi,
+  //   wallet
+  // );
 
   // Check if the citizen is already registered
-  const isRegistered =
-    (await reputationRegistryContract.getEXP(citizenAddress)) > 0;
+  // const isRegistered =
+  //   (await reputationRegistryContract.getEXP(citizenAddress)) > 0;
 
-  // If citizen is already registered, return a message indicating so
-  if (isRegistered) {
-    return { success: false, message: "Citizen is already registered" };
-  }
+   // If citizen is already registered, return a message indicating so
+  // if (isRegistered) {
+  //   return { success: false, message: "Citizen is already registered" };
+  // }
 
   // Check if the referrer address is valid before calling getEXP
-  let referrerEXP = 0;
-  if (
-    ethers.isAddress(referrerAddress) &&
-    referrerAddress !== ethers.ZeroAddress
-  ) {
-    referrerEXP = await reputationRegistryContract.getEXP(referrerAddress);
-  }
-  const effectiveReferrerAddress =
-    referrerEXP > 0 ? referrerAddress : ethers.ZeroAddress;
+  // let referrerEXP = 0;
+  // if (
+  //   ethers.isAddress(referrerAddress) &&
+  //   referrerAddress !== ethers.ZeroAddress
+  // ) {
+  //   referrerEXP = await reputationRegistryContract.getEXP(referrerAddress);
+  // }
+  // const effectiveReferrerAddress =
+  //   referrerEXP > 0 ? referrerAddress : ethers.ZeroAddress;
 
   // Register the citizen
-  const transaction = await reputationRegistryContract.registerCitizen(
-    citizenAddress,
-    effectiveReferrerAddress
-  );
-  await transaction.wait();
+  // const transaction = await reputationRegistryContract.registerCitizen(
+  //   citizenAddress,
+  //   effectiveReferrerAddress
+  // );
+  // await transaction.wait();
 
   return { success: true };
 };
