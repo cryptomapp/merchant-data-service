4c778861b03f786262f0b128deffeac418efa546
twentyone37 <twentyone_37@proton.me>
2023-12-02 09:04:24 +0100

    Update env variable

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(cors());
 
 app.use(
   cors({
-    origin: ["https://cryptomapp.vercel.app", "http://localhost:9000"],
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
